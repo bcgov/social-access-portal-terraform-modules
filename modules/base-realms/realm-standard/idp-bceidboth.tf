8ce220a47176775aa019fa0f6031b44e3b348040
Simen Fivelstad Smaaberg <simen@freshworks.io>
2022-05-19 14:10:36 -0700

    BCSCP-4 Removed auth from OIDC urls

--- modules/base-realms/realm-standard/idp-bceidboth.tf
+++ modules/base-realms/realm-standard/idp-bceidboth.tf
@@ -2,10 +2,10 @@ module "bceidboth_idp" {
   source            = "../../oidc-idp"
   realm_id          = module.realm.id
   alias             = var.bceidboth_realm_name
-  authorization_url = "${var.keycloak_url}/auth/realms/${var.bceidboth_realm_name}/protocol/openid-connect/auth"
-  token_url         = "${var.keycloak_url}/auth/realms/${var.bceidboth_realm_name}/protocol/openid-connect/token"
-  user_info_url     = "${var.keycloak_url}/auth/realms/${var.bceidboth_realm_name}/protocol/openid-connect/userinfo"
-  jwks_url          = "${var.keycloak_url}/auth/realms/${var.bceidboth_realm_name}/protocol/openid-connect/certs"
+  authorization_url = "${var.keycloak_url}/realms/${var.bceidboth_realm_name}/protocol/openid-connect/auth"
+  token_url         = "${var.keycloak_url}/realms/${var.bceidboth_realm_name}/protocol/openid-connect/token"
+  user_info_url     = "${var.keycloak_url}/realms/${var.bceidboth_realm_name}/protocol/openid-connect/userinfo"
+  jwks_url          = "${var.keycloak_url}/realms/${var.bceidboth_realm_name}/protocol/openid-connect/certs"
   client_id         = var.bceidboth_client_id
   client_secret     = var.bceidboth_client_secret
 }
